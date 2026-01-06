11902d79d746cf593f07c0bc67a832f7ffc4988d
Andres Morales <meliand11@yahoo.com>
2026-01-06 14:44:19 -0500

    this shows how to return function to other functions

--- main.go
+++ main.go
@@ -13,11 +13,22 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	double := transformNumbers(&numbers, double)
 	triple := transformNumbers(&numbers, triple)
 
 	fmt.Println(double)
 	fmt.Println(triple)
+
+	transformFn1 := getTransformerFunction(&numbers)
+	transformFn2 := getTransformerFunction(&moreNumbers)
+
+	transformNumbers1 := transformNumbers(&numbers, transformFn1)
+
+	moreTransformedNumbers := transformNumbers(&numbers, transformFn2)
+
+	fmt.Println(transformNumbers1)
+	fmt.Println(moreTransformedNumbers)
 }
 
 /*
@@ -32,6 +43,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	}
 
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
